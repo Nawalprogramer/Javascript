b0e33e7e9811e7f74b675ee2bdc9382697f14889
~Nawal <m.nawal8098@gmail.com>
2024-09-11 20:49:31 +0500

    Add files via upload

---
+++ nawal/form.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+        // let word=document.getElementById("search").value;
+        // let reg=new RegExp(word , 'gi');
+        // let str = "i study in aptech";       
+        // let result=str.match(reg);
+        // document.getElementById("output").innerHTML="match words="+(result|| "no match found");
+    }
